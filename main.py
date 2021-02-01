c3ff7a993e8fa2fb8189950ea06b43dd2842a039
Evan Huang <evanhuang117@gmail.com>
2021-02-01 13:20:30 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import PyPDF2
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
