34a61e1e86218ffacf3e65d8698d3ffeea03e972
Evan Huang <evanhuang117@gmail.com>
2021-02-02 15:27:42 -0500

    implement basic text summarization without coreference

--- main.py
+++
@@ -1,9 +0,0 @@
-import PyPDF2
-
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ summarize.py
@@ -0,0 +1,50 @@
+import pdfplumber
+import sys
+from summarizer import Summarizer
+from summarizer.coreference_handler import CoreferenceHandler
+from fpdf import FPDF
+import spacy
+
+def use_coreference():
+    spacy.load('en_core_web_lg')
+    handler = CoreferenceHandler(spacy_model='en_core_web_lg')
+    return Summarizer(sentence_handler=handler)
+
+# 1. activate python venv "source venv/bin/activate"
+# 2. run "python3 summarize.py 'path to PDF' "
+file_name = sys.argv[1]
+use_coref = False
+pdf = pdfplumber.open(file_name)
+print("Imported a PDF with " + str(len(pdf.pages)) + " pages")
+
+# read in the text
+text = ""
+for pg in pdf.pages:
+    print("Processing page " + str(pg.page_number))
+    text += pg.extract_text()
+
+# run the summarizer
+if use_coref:
+    model = use_coreference()
+else:
+    model = Summarizer()
+
+summarized = model(body=text)
+
+''' uncomment to print to text file
+with open("summarized", 'w') as f:
+    f.write(summarized)
+'''
+
+
+# output the summarized text to a pdf
+output_pdf = FPDF()
+output_pdf.add_page()
+output_pdf.set_font("Arial", size=12)
+# fix encoding issues with text
+summarized = summarized.encode('latin-1', 'replace').decode('utf-8')
+# multi_cell auto line breaks (wrap text)
+output_pdf.multi_cell(200, 10, summarized, align='L')
+output_pdf.output(file_name + "-summarized.pdf")
+
+
